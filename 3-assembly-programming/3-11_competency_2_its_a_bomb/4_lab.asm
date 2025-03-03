01ee638114942e71bdc9a66913eb82c8dbee21f9
Joey Crawford <joseph.d.crawford27.mil@us.navy.mil>
2025-03-03 08:46:22 -0500

    Clarify >> vs >>> notation in asm file lab 4

--- 3-assembly-programming/3-11_competency_2_its_a_bomb/4_lab.asm
+++ 3-assembly-programming/3-11_competency_2_its_a_bomb/4_lab.asm
@@ -3,8 +3,8 @@
 ;;
 ;; 3-11 Competency 2 Its a Bomb! - Competency Lab
 ;;
-;; This file is module specific and houses assembly application labs for 
-;; students. Students should edit this file and run `make` to test their 
+;; This file is module specific and houses assembly application labs for
+;; students. Students should edit this file and run `make` to test their
 ;; solutions.
 ;;
 ;; @file        4_lab.nasm
@@ -17,17 +17,17 @@
 ;; Student TODO:    Complete the lab assignment below.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  
+;;
 ;;        __       _________  __      __
 ;;       /\ \     /________/ |\ \    /| |
 ;;      / /\ \    | |______  | \ \  / | |
 ;;     /_/__\ \   |/______/| | |\ \/ /| |
 ;;    /______\ \   ______| | | | \/_/ | |
 ;;   /_/      \_\ /______|/  |_|      |_|
-;; 
+;;
 ;; The basics of the Linux x64 calling convention:
 ;;
-;;     - Arguments are passed in `rdi`, `rsi`, `rdx`, `rcx`, `r8`, `r9`, in 
+;;     - Arguments are passed in `rdi`, `rsi`, `rdx`, `rcx`, `r8`, `r9`, in
 ;;       that order and any additional arguments are passed on the stack.
 ;;     - The return value is stored in `rax`.
 ;;
@@ -56,23 +56,23 @@ global studentFunction10
 
 
 ; 1. Timer Deactivation Code.
-; 
+;
 ; unsigned long long studentFunction1(
-;   unsigned long long a, 
+;   unsigned long long a,
 ;   unsigned long long b,
 ;   unsigned long long c
 ; );
 ;
-; @brief    Function calculates and returns the timer deactivation code given 
-;           three input combination codes (as `long long`s). The combinations 
-;           run through circuits that use arithmetic, bitwise, and boolean 
+; @brief    Function calculates and returns the timer deactivation code given
+;           three input combination codes (as `long long`s). The combinations
+;           run through circuits that use arithmetic, bitwise, and boolean
 ;           operations where Circuit `A` executes first, then `B`, the `C`.
 ;
 ;           Circuit A executes the following:
 ;               `A = ( (A*B + A*C + B*C) % C ) + 0xdeadbeef - 0x60a7 + 0xca7`.
-;       
+;
 ;           Circuit B executes the following:
-;               `B = ( ((A << 8) >>> 22) // 8 ) 
+;               `B = ( ((A << 8) >>> 22) // 8 )
 ;               + (B >> (C & 0xf))
 ;               + ( (C >> 3) <<< 42 ) + 0x20d1ac`.
 ;
@@ -93,12 +93,13 @@ global studentFunction10
 ; RULE: Do NOT use jumps or loops.
 ; ASSUME:   Division by zero will not occur.
 ; ASSUME:   All divisions are integer divisions.
+; ASSUME:   In our notation: ">>" is logical shift, ">>>" is rotate
 ;
 studentFunction1:
 
     ; --- Add your solution below this line -----------------------------------
 
-    
+
 
     ; --- Do not edit below this line -----------------------------------------
     ret
