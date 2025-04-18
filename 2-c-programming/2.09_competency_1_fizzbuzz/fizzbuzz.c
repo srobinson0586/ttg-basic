8c21e372735852e0d49b73678d57b4606de411ab
skyler <skyler@skylers-MacBook-Air.local>
2025-04-18 10:23:59 -0400

    2.09

--- 2-c-programming/2.09_competency_1_fizzbuzz/fizzbuzz.c
+++ 2-c-programming/2.09_competency_1_fizzbuzz/fizzbuzz.c
@@ -7,6 +7,12 @@ void fizzBuzz(int n) {
     //       Buzz for numbers divisible by 5, 
     //       FizzBuzz for numbers divisible by both 3 and 5,
     //       and the number itself for all other cases.
+    for (int i = 0; i <= n; i++) {
+        if (i % 3 == 0) printf("Fizz");
+        if (i % 5 == 0) printf("Buzz");
+        if (i % 3 != 0 && i % 5 != 0) printf("%d", i);
+        printf("\n");
+    }
 
 
 }
@@ -17,6 +23,7 @@ int main() {
 
     // TODO: Call fizzBuzz() in a loop from 1 to the number that the user has entered as the parameter.
     // Example: if the user entered 20, fizzBuzz() should run for every number between 1 to 20.
-
+    int num = get_int("Input: ");
+    fizzBuzz(num);
     return 0;
 }
